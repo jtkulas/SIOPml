0e7748a02a341798e1605476c425475be2377a52
John-o Kulas <kulasj@montclair.edu>
2023-02-19 16:32:37 -0600

    hyperlink in script

---
+++ Boston2023/Kulas.R
@@ -0,0 +1,7 @@
+## SIOP 2023 Machine Learning Competition
+
+## description is whack/poor: https://eval.ai/web/challenges/challenge-page/1937/overview
+
+## Alicia recommended qualitative resource: https://www.thematicanalysis.net/
+
+train <- read.csv("C:\\Kulas\\Git\\SIOPml\\Boston2023\\train_pub.csv")
