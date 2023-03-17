8fc715647cc9835c6ecb4eede8789c8298b3134a
heyimMIKE123 <itsmefob@gmail.com>
2023-03-17 02:31:26 -0400

    Update idk.R

--- Boston2023/DeFabiis/idk.R
+++ Boston2023/DeFabiis/idk.R
@@ -23,3 +23,22 @@ test %>%
 
 
 
+
+##### Paragraphs #####
+test <- train %>% unnest_tokens(word, text_exercise_4, token = "paragraphs") %>% select(word)
+
+test %>% filter(word %in% as.character(bing_pos))
+
+# Count number of positive words in a paragraph
+p <- 1
+h <- 0
+for (i in bing_pos) {
+  if (str_detect(as.character(test[p,]), as.character(bing_pos[p,1]))) {
+    h + 1
+  }
+  else{
+    next
+  }
+  return(h)
+}
+
